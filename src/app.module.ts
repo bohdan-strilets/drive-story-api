0326bd4f62d99e39ca50962fc34a3cb32e2b0826
BS <bohdan.strilets@gmail.com>
2025-01-06 14:16:37 +0100

    Created a connection to the database.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_URL),
+  ],
 })
 export class AppModule {}

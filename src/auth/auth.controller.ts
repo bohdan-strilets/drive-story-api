281c695b0143c796acfa1b692a624014f8bcac6c
BS <bohdan.strilets@gmail.com>
2025-01-06 17:03:28 +0100

    Added cookie handling.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { ApiResponse } from 'src/helpers/api-response.type';
+import { cookieKeys, cookieOptions } from 'src/helpers/cookie-options';
 import { AuthService } from './auth.service';
 import { RegistrationDto } from './dto/registration.dto';
 import { AuthResponse } from './types/auth-response.type';
@@ -11,8 +13,16 @@ export class AuthController {
   @Post('registration')
   async registration(
     @Body() dto: RegistrationDto,
+    @Res({ passthrough: true }) res: Response,
   ): Promise<ApiResponse<AuthResponse> | ResponseType> {
     const data = await this.authService.registration(dto);
+
+    if (data.success) {
+      const refreshToken = data.data?.tokens.refreshToken;
+      res.cookie(cookieKeys.REFRESH_TOKEN, refreshToken, cookieOptions);
+    } else {
+      res.status(data.statusCode);
+    }
 
     return data;
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  app.enableCors({ origin: process.env.CLIENT_URL, credentials: true });
+  app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT || 4040);
 }

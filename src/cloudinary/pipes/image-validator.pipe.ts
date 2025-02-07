abc9ed9adb5d7282f427c1bc96bdc0760ae0882a
BS <bohdan.strilets@gmail.com>
2025-02-07 19:42:50 +0100

    Improved validation for files.

--- src/cloudinary/pipes/image-validator.pipe.ts
+++ src/cloudinary/pipes/image-validator.pipe.ts
@@ -1,5 +1,6 @@
 import {
   FileTypeValidator,
+  HttpStatus,
   MaxFileSizeValidator,
   ParseFilePipe,
 } from '@nestjs/common';
@@ -8,6 +9,8 @@ const allowedFileTypes = /(jpg|jpeg|png|webp)/;
 const maxSize = 8 * 1024 * 1024;
 
 export const imageValidator = new ParseFilePipe({
+  errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+  fileIsRequired: true,
   validators: [
     new MaxFileSizeValidator({ maxSize }),
     new FileTypeValidator({ fileType: allowedFileTypes }),

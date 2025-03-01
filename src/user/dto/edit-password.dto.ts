272c1f2c1fbccca551fff1a32d1da60e8b81666e
BS <bohdan.strilets@gmail.com>
2025-03-01 15:46:58 +0100

    Fixed a password length.

--- src/user/dto/edit-password.dto.ts
+++ src/user/dto/edit-password.dto.ts
@@ -4,8 +4,8 @@ import { passwordRegex } from 'src/password/regex/password.regex';
 export class EditPasswordDto {
   @IsNotEmpty({ message: 'Password must not be empty' })
   @IsString({ message: 'Password must be a string' })
-  @Length(2, 12, {
-    message: 'Password must be between 2 and 12 characters long',
+  @Length(6, 12, {
+    message: 'Password must be between 6 and 12 characters long',
   })
   @Matches(passwordRegex, {
     message: 'Password must contain at least one letter and one number',
@@ -14,8 +14,8 @@ export class EditPasswordDto {
 
   @IsNotEmpty({ message: 'New password must not be empty' })
   @IsString({ message: 'New password must be a string' })
-  @Length(2, 12, {
-    message: 'New password must be between 2 and 12 characters long',
+  @Length(6, 12, {
+    message: 'New password must be between 6 and 12 characters long',
   })
   @Matches(passwordRegex, {
     message: 'New password must contain at least one letter and one number',
--- src/user/dto/reset-password.dto.ts
+++ src/user/dto/reset-password.dto.ts
@@ -4,8 +4,8 @@ import { passwordRegex } from 'src/password/regex/password.regex';
 export class ResetPasswordDto {
   @IsNotEmpty({ message: 'Password must not be empty' })
   @IsString({ message: 'Password must be a string' })
-  @Length(2, 12, {
-    message: 'Password must be between 2 and 12 characters long',
+  @Length(6, 12, {
+    message: 'Password must be between 6 and 12 characters long',
   })
   @Matches(passwordRegex, {
     message: 'Password must contain at least one letter and one number',

4f0e20572f499be7f913a510395fbec00da0de61
BS <bohdan.strilets@gmail.com>
2025-03-21 17:17:53 +0100

    Changed the answer for the password recovery method.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -83,9 +83,7 @@ export class UserController {
     const clientUrl = this.configService.get('CLIENT_URL');
 
     if (data.success) {
-      res.redirect(`${clientUrl}/reset-password?valid=true`);
-    } else {
-      res.redirect(`${clientUrl}/reset-password?valid=false`);
+      res.redirect(`${clientUrl}/reset-password`);
     }
 
     return data;
